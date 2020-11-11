414ca13f644419b3f1c0b022bbf5bd5243fad0c1
Fabrício Wagomacker <fabricio.gwr@gmail.com>
2020-11-11 17:00:26 -0400

    criando repository e service para Categoria

---
+++ src/main/java/com/fgwr/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.fgwr.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fgwr.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
