ccbfc45232e4666205f382c55ad0c6ef57bf4fc8
Fabrício Wagomacker <fabricio.gwr@gmail.com>
2020-11-12 19:18:25 -0400

    Pedido, pagamento e estado do pagamento

---
+++ src/main/java/com/fgwr/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.fgwr.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fgwr.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+	
+
+}
