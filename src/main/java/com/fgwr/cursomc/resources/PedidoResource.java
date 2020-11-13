8987185a8fafbce7e7d50a638264caa04f6483d2
Fabrício Wagomacker <fabricio.gwr@gmail.com>
2020-11-12 22:28:53 -0400

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/fgwr/cursomc/resources/PedidoResource.java
+++ src/main/java/com/fgwr/cursomc/resources/PedidoResource.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fgwr.cursomc.domain.Cliente;
-import com.fgwr.cursomc.services.ClienteService;
+import com.fgwr.cursomc.domain.Pedido;
+import com.fgwr.cursomc.services.PedidoService;
 
 @RestController
-@RequestMapping(value="/clientes")
-public class ClienteResource {
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
 	
 	@Autowired
-	private ClienteService service;
+	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Cliente obj = service.find(id);
+		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
