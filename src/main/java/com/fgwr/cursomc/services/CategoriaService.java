f9c3f505cd18a16a58c7286fb2afdb4949f64ab3
Fabrício Wagomacker <fabricio.gwr@gmail.com>
2020-11-16 10:51:57 -0400

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/fgwr/cursomc/services/CategoriaService.java
+++ src/main/java/com/fgwr/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fgwr.cursomc.domain.Categoria;
@@ -39,8 +42,7 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 
 		}
@@ -48,5 +50,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
